dab17db56133a2b191467b13706ae39f4ea40e7e
davidrpugh <drpugh@protonmail.com>
2021-07-01 22:35:30 +0300

    Updated configuration files

--- bin/create-conda-env.sh
+++ bin/create-conda-env.sh
@@ -4,6 +4,4 @@ set -euo pipefail
 
 # create the conda environment
 export ENV_PREFIX=$PWD/env
-conda env create --prefix $ENV_PREFIX --file environment.yml --force
-conda activate $ENV_PREFIX
-source postBuild
+mamba env create --prefix $ENV_PREFIX --file environment.yml --force
